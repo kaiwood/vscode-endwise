c251f21223221084c695fbc86d8a6264c14b880c
Kai Wood <kwood@kwd.io>
2017-05-25 16:46:10 +0200

    Add case

--- src/extension.ts
+++ src/extension.ts
@@ -44,7 +44,7 @@ async function endwiseEnter() {
 function shouldAddEnd(lineText) {
     const trimmedText: string = lineText.trim();
     const startsWithConditions: string[] = [
-        "if", "unless", "while", "for", "do", "def", "class", "module"
+        "if", "unless", "while", "for", "do", "def", "class", "module", "case"
     ];
 
     for (let condition of startsWithConditions) {
