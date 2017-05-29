9b7f5a4df08cedf39bed1b25162894310cbd55cf
Kai Wood <kwood@kwd.io>
2017-05-29 11:17:19 +0200

    Do not add additional "end"'s, if already present

--- src/extension.ts
+++ src/extension.ts
@@ -10,13 +10,13 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-    let enter = vscode.commands.registerCommand('endwise.enter', () => {
-        endwiseEnter();
+    let enter = vscode.commands.registerCommand('endwise.enter', async () => {
+        await endwiseEnter();
     });
 
     let cmdEnter = vscode.commands.registerCommand('endwise.cmdEnter', async () => {
         await vscode.commands.executeCommand('cursorEnd');
-        endwiseEnter(true);
+        await endwiseEnter(true);
     });
 
     context.subscriptions.push(enter);
@@ -26,10 +26,16 @@ async function endwiseEnter(calledWithModifier = false) {
     const editor: vscode.TextEditor = vscode.window.activeTextEditor;
     const lineNumber: number = editor.selection.active.line;
     const columnNumber: number = editor.selection.active.character;
+    const lineCount: number = editor.document.lineCount;
     const lineText: string = editor.document.lineAt(lineNumber).text;
     const lineLength: number = lineText.length;
+    const possibleClosings = [];
+    for (let i = lineNumber; i < lineCount; ++i) {
+        let additionalLine = await editor.document.lineAt(i);
+        possibleClosings.push(additionalLine.text);
+    }
 
-    if (shouldAddEnd(lineText, columnNumber, calledWithModifier)) {
+    if (shouldAddEnd(lineText, columnNumber, calledWithModifier, possibleClosings)) {
         await editor.edit((textEditor) => {
             textEditor.insert(new vscode.Position(lineNumber, lineLength), `\n${indentationFor(lineText)}end`);
         });
@@ -37,7 +43,7 @@ async function endwiseEnter(calledWithModifier = false) {
         vscode.commands.executeCommand('editor.action.insertLineAfter');
     } else if (shouldUnindent(lineText)) {
         await vscode.commands.executeCommand('editor.action.outdentLines');
-        editor.edit((textEditor) => {
+        await editor.edit((textEditor) => {
             textEditor.insert(new vscode.Position(lineNumber, lineLength), `\n${indentationFor(lineText)}`);
         });
     } else {
@@ -48,11 +54,21 @@ async function endwiseEnter(calledWithModifier = false) {
     }
 }
 
-function shouldAddEnd(lineText, columnNumber, calledWithModifier) {
+/**
+ * Check if a closing "end" should be set
+ */
+function shouldAddEnd(lineText, columnNumber, calledWithModifier, possibleClosings) {
 
     // Do not add "end" if enter is pressed in the middle of a line, *except* when a modifier key is used
     if (!calledWithModifier && lineText.length > columnNumber) {
-        return false
+        return false;
+    }
+
+    // Do not add a closing "end" if there is one on the same indentaion level
+    for (let closing of possibleClosings) {
+        if (closing === indentationFor(lineText) + "end") {
+            return false;
+        }
     }
 
     const trimmedText: string = lineText.trim();
@@ -70,6 +86,9 @@ function shouldAddEnd(lineText, columnNumber, calledWithModifier) {
     return false;
 }
 
+/**
+ * Check if the line should unindent because of other keywords
+ */
 function shouldUnindent(lineText) {
     const trimmedText: string = lineText.trim();
     const unindentConditions: string[] = [
@@ -83,6 +102,9 @@ function shouldUnindent(lineText) {
     return false;
 }
 
+/**
+ * Get indentation level of the previous line
+ */
 function indentationFor(lineText) {
     const trimmedLine: string = lineText.trim();
     if (trimmedLine.length === 0) return lineText;
