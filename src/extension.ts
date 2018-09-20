b9b5fbb69592f312b8b8e54ff3e5e5a691a3f9b3
Kai Wood <kwood@kwd.io>
2018-09-20 11:15:06 +0200

    Inline functions so we don't need to pass the parameters around

--- src/extension.ts
+++ src/extension.ts
@@ -8,12 +8,15 @@
 "use strict";
 import * as vscode from "vscode";
 
+/**
+ * Activate plugin commands
+ */
 export function activate(context: vscode.ExtensionContext) {
-  let enter = vscode.commands.registerCommand("endwise.enter", async () => {
+  const enter = vscode.commands.registerCommand("endwise.enter", async () => {
     await endwiseEnter();
   });
 
-  let cmdEnter = vscode.commands.registerCommand(
+  const cmdEnter = vscode.commands.registerCommand(
     "endwise.cmdEnter",
     async () => {
       await vscode.commands.executeCommand("cursorEnd");
@@ -23,7 +26,7 @@ export function activate(context: vscode.ExtensionContext) {
 
   // We have to check "acceptSuggestionOnEnter" is set to a value !== "off" if the suggest widget is currently visible,
   // otherwise the suggestion won't be triggered because of the overloaded enter key.
-  let checkForAcceptSelectedSuggestion = vscode.commands.registerCommand(
+  const checkForAcceptSelectedSuggestion = vscode.commands.registerCommand(
     "endwise.checkForAcceptSelectedSuggestion",
     async () => {
       const config = vscode.workspace.getConfiguration();
@@ -42,120 +45,137 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(checkForAcceptSelectedSuggestion);
 }
 
+/**
+ * The plugin itself
+ */
 async function endwiseEnter(calledWithModifier = false) {
   const editor: vscode.TextEditor = vscode.window.activeTextEditor;
   const lineNumber: number = editor.selection.active.line;
   const columnNumber: number = editor.selection.active.character;
-  const lineCount: number = editor.document.lineCount;
   const lineText: string = editor.document.lineAt(lineNumber).text;
   const lineLength: number = lineText.length;
 
-  if (
-    shouldAddEnd(lineText, columnNumber, lineNumber, calledWithModifier, editor)
-  ) {
+  if (shouldAddEnd()) {
+    await linebreakWithClosing();
+  } else {
+    await linebreak();
+  }
+
+  /**
+   * Insert a line break, add the correct closing and correct cursor position
+   */
+  async function linebreakWithClosing() {
     await editor.edit(textEditor => {
       textEditor.insert(
         new vscode.Position(lineNumber, lineLength),
         `\n${indentationFor(lineText)}end`
       );
     });
+
     await vscode.commands.executeCommand("cursorUp");
     vscode.commands.executeCommand("editor.action.insertLineAfter");
-  } else {
+  }
+
+  /**
+   * Insert a linebreak, no closing, correct cursor position
+   */
+  async function linebreak() {
     await vscode.commands.executeCommand("lineBreakInsert");
     if (columnNumber === lineText.length) {
       await vscode.commands.executeCommand("cursorEnd");
       await vscode.commands.executeCommand("cursorWordStartRight");
     } else {
       await vscode.commands.executeCommand("cursorRight");
+
       let newLine = await editor.document.lineAt(editor.selection.active.line)
         .text;
+
       if (newLine[1] === " " && newLine.trim().length > 0) {
         await vscode.commands.executeCommand("cursorWordEndRight");
         await vscode.commands.executeCommand("cursorHome");
       }
     }
   }
-}
 
-/**
- * Check if a closing "end" should be set
- */
-function shouldAddEnd(lineText, columnNumber, lineNumber, calledWithModifier, editor) {
-  const openings = [
-    /^\s*?if(\s|\()/,
-    /^\s*?unless(\s|\()/,
-    /^\s*?while(\s|\()/,
-    /^\s*?for(\s|\()/,
-    /\s?do(\s?$|\s\|.*\|\s?$)/,
-    /^\s*?def\s/,
-    /^\s*?class\s/,
-    /^\s*?module\s/,
-    /^\s*?case(\s|\()/,
-    /^\s*?begin\s/,
-    /^\s*?until(\s|\()/
-  ];
+  /**
+   * Check if a closing "end" should be set. Pretty much the meat of this plugin.
+   */
+  function shouldAddEnd() {
+    const openings = [
+      /^\s*?if(\s|\()/,
+      /^\s*?unless(\s|\()/,
+      /^\s*?while(\s|\()/,
+      /^\s*?for(\s|\()/,
+      /\s?do(\s?$|\s\|.*\|\s?$)/,
+      /^\s*?def\s/,
+      /^\s*?class\s/,
+      /^\s*?module\s/,
+      /^\s*?case(\s|\()/,
+      /^\s*?begin\s/,
+      /^\s*?until(\s|\()/
+    ];
 
-  const singleLineDefCondition = /;\s*end[\s;]*$/;
+    const singleLineDefCondition = /;\s*end[\s;]*$/;
 
-  const currentIndentation = indentationFor(lineText);
+    const currentIndentation = indentationFor(lineText);
 
-  // Do not close if enter key is pressed in the middle of a line, *except* when a modifier key is used
-  if (!calledWithModifier && lineText.length > columnNumber) return false;
-  // Also, do not close on single line definitions
-  if (lineText.match(singleLineDefCondition)) return false;
+    // Do not close if enter key is pressed in the middle of a line, *except* when a modifier key is used
+    if (!calledWithModifier && lineText.length > columnNumber) return false;
+    // Also, do not close on single line definitions
+    if (lineText.match(singleLineDefCondition)) return false;
 
-  for (let condition of openings) {
-    if (!lineText.match(condition)) continue;
+    for (let condition of openings) {
+      if (!lineText.match(condition)) continue;
 
-    const LIMIT = 100000;
-    let stackCount = 0;
-    let documentLineCount = editor.document.lineCount
+      const LIMIT = 100000;
+      let stackCount = 0;
+      let documentLineCount = editor.document.lineCount;
 
-    // Do not add "end" if code structure is already balanced
-    for (let ln = lineNumber; ln <= lineNumber + LIMIT; ln++) {
-      // Close if we are at the end of the document
-      if (documentLineCount <= ln + 1) return true;
+      // Do not add "end" if code structure is already balanced
+      for (let ln = lineNumber; ln <= lineNumber + LIMIT; ln++) {
+        // Close if we are at the end of the document
+        if (documentLineCount <= ln + 1) return true;
 
-      let line = editor.document.lineAt(ln + 1).text;
-      let lineStartsWithEnd = line.trim().startsWith("end");
+        let line = editor.document.lineAt(ln + 1).text;
+        let lineStartsWithEnd = line.trim().startsWith("end");
 
-      // Always close the statement if there is another closing found on a smaller indentation level
-      if (currentIndentation > indentationFor(line) && lineStartsWithEnd) return true;
-
-      if (currentIndentation === indentationFor(line)) {
-        // If another opening is found, increment the stack counter
-        for (let innerCondition of openings) {
-          if (line.match(innerCondition)) {
-            stackCount += 1;
-            break;
-          }
+        // Always close the statement if there is another closing found on a smaller indentation level
+        if (currentIndentation > indentationFor(line) && lineStartsWithEnd) {
+          return true;
         }
 
-        if (lineStartsWithEnd && stackCount > 0) {
-          stackCount -= 1;
-          continue;
-        } else if (lineStartsWithEnd) {
-          return false;
+        if (currentIndentation === indentationFor(line)) {
+          // If another opening is found, increment the stack counter
+          for (let innerCondition of openings) {
+            if (line.match(innerCondition)) {
+              stackCount += 1;
+              break;
+            }
+          }
+
+          if (lineStartsWithEnd && stackCount > 0) {
+            stackCount -= 1;
+            continue;
+          } else if (lineStartsWithEnd) {
+            return false;
+          }
         }
       }
     }
+
+    return false;
   }
 
-  return false;
+  /**
+   * Helper to get indentation level of the previous line
+   */
+  function indentationFor(lineText) {
+    const trimmedLine: string = lineText.trim();
+    if (trimmedLine.length === 0) return lineText;
+
+    const whitespaceEndsAt: number = lineText.indexOf(trimmedLine);
+    const indentation: string = lineText.substr(0, whitespaceEndsAt);
+
+    return indentation;
+  }
 }
-
-/**
- * Get indentation level of the previous line
- */
-function indentationFor(lineText) {
-  const trimmedLine: string = lineText.trim();
-  if (trimmedLine.length === 0) return lineText;
-
-  const whitespaceEndsAt: number = lineText.indexOf(trimmedLine);
-  const indentation: string = lineText.substr(0, whitespaceEndsAt);
-
-  return indentation;
-}
-
-export function deactivate() {}
